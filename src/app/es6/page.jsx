48c0e81965586867ed40aa6e6402052582a0aba9
Munkhdalai-G <Bylzuuhai7@gmail.com>
2026-01-05 19:37:41 +0800

    birthdaylist

--- src/app/es6/page.jsx
+++ src/app/es6/page.jsx
@@ -1,6 +1,7 @@
 "use client";
-import { Scripts } from "@/components/Scripts";
-import { App } from "../../components/App";
+// import { Scripts } from "@/components/Scripts";
+// import { App } from "../../components/App";
+// import { URL } from "next/dist/compiled/@edge-runtime/primitives/url";
 // export default function Home() {
 //   return <ColorList />;
 // }
@@ -56,9 +57,85 @@ import { App } from "../../components/App";
 //   );
 // }
 
+// export default function Home() {
+//   return <Status stock={0} />;
+// }
+// const Status = ({ stock }) => (
+//   <div className="bg-amber-400">{stock > 0 ? "in stock" : "out of stock"}</div>
+// );
+
 export default function Home() {
-  return <Status stock={0} />;
+  return (
+    <div className="flex justify-center items-center h-screen bg-pink-300 ">
+      <BirthdayList />
+    </div>
+  );
 }
-const Status = ({ stock }) => (
-  <div className="bg-amber-400">{stock > 0 ? "in stock" : "out of stock"}</div>
-);
+
+const Birthday = [
+  {
+    id: 1,
+    name: "Bold",
+    age: 15,
+    image: "https://avatar.iran.liara.run/public",
+  },
+
+  {
+    id: 2,
+    name: "Bat",
+    age: 15212,
+    image: "https://avatar.iran.liara.run/public",
+  },
+
+  {
+    id: 3,
+    name: "chimeg",
+    age: 1526,
+    image: "",
+  },
+
+  {
+    id: 4,
+    name: "dddddd",
+    age: 131,
+    image: "",
+  },
+
+  {
+    id: 5,
+    name: "Boldssssssssss",
+    age: 20,
+    image: "",
+  },
+];
+
+const BirthdayList = () => {
+  return (
+    <div className=" bg-white p-10">
+      <h1 className="font-semibold mb-8 text-3xl text-black">
+        {Birthday.length} birthdays today
+      </h1>
+      <div className="flex flex-col gap-4">
+        {Birthday.map(({ id, age, name, image }) => (
+          <BirthdayListItem key={id} name={name} age={age} image={image} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const BirthdayListItem = () => {
+  return (
+    <div className="flex gap-4 items-center w-134">
+      <img
+        className="w-20 h-20 rounded-full"
+        src={Birthday.image}
+        alt="profile"
+      />
+      <div>
+        <h2 className="text-2xl font-bold">{Birthday.name}</h2>
+        <p className="text-gray-500">{Birthday.age}</p>
+      </div>
+    </div>
+  );
+};
